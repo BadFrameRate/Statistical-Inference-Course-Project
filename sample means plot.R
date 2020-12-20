54cdd634ca8c67beeb06be6fb155012668c5f5f0
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-12-20 21:11:05 +0530

    sample means plot

---
+++ sample means plot.R
@@ -0,0 +1,10 @@
+set.seed(137)
+
+q<-ggplot(as.data.frame(exp_means), aes(x=exp_means, y=..density..)) +
+  geom_histogram(binwidth=0.15) +
+  labs(x="Means of 40 Exponential Random Variables", y="Density")
+
+q<-q + geom_line(data=data.frame(x=exp_means ,y=dnorm(exp_means,5,5/sqrt(40))),
+                 aes(x, y), col="blue")
+
+print(q)
